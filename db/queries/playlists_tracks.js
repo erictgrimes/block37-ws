fe36529adcfc2ba80282399d3b6707447dd9b413
Eric Grimes <erictgrimes@gmail.com>
2025-08-07 11:36:10 -0700

    fixed all 3 queries

--- db/queries/playlists_tracks.js
+++ db/queries/playlists_tracks.js
@@ -1,10 +1,5 @@
 import db from "#db/client";
 
-export async function getPlaylists_tracks() {
-  const sql = `SELECT * FROM playlists_tracks`;
-  const { rows: playlists_tracks } = await db.query(sql);
-  return playlists_tracks;
-}
 
 export async function createPlaylists_tracks(playlistId, trackId) {
   const sql = `INSERT INTO playlists_tracks (playlist_id, track_id)
